920e7e92d50bccba43f7c5709ec352918780297f
felipetdsa <felipetdsa@gmail.com>
2015-05-28 10:15:54 -0300

    Create Marathon.java

---
+++ Marathon.java
@@ -0,0 +1,46 @@
+/*
+ *Assignment 3
+ *Find the best and second best times.
+ *Print the best and second best names and index.
+ *
+ *By felipetdsa@gmail.com
+ * PS: I will make another version of program called Marathon2.java
+ *        to try another methods.
+ */
+
+
+class Marathon {
+	public static void main(String[] arguments) {
+		String[] names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil",
+				"Matt", "Alex", "Emma", "John", "James", "Jane", "Emily",
+				"Daniel", "Neda", "Aaron", "Kate" };
+
+		int[] times = { 341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412,
+				393, 299, 343, 317, 265 };
+
+		int bestTime = 0;
+		int secondBest = 0;
+		int diff = 0;
+
+		for (int i = 0; i < names.length; i++) {
+			if (times[i] < times[bestTime]) {
+				bestTime = i;
+			}
+		}
+		for (int i = 0; i < names.length; i++){
+			if (i == bestTime){
+				i++;				
+			}else if (i == 0){
+				diff = times[i] - times[bestTime];
+			}
+			if (times[i] - times[bestTime] < diff){
+				diff = times[i] - times[bestTime];
+				secondBest = i;
+			}
+		}
+		System.out.println("Best Time is " + names[bestTime] + ": " + times[bestTime]);
+		System.out.println("Index: " + bestTime);
+		System.out.println("Second Best is " + names[secondBest] + ": " + times[secondBest]);
+		System.out.println("Index: " + secondBest);
+	}
+}
